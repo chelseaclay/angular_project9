d69b2f84d722a35bddf15796e7b0c27ace78d377
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2017-09-30 23:06:07 +0200

    category not working re-direct not working

---
+++ public/scripts/controllers/recipe-detail.controller.js
@@ -0,0 +1,69 @@
+(function () {
+    'use strict';
+    angular
+        .module('app')
+        .controller('RecipeDetailController', RecipeDetailController);
+
+    function RecipeDetailController($scope, dataService, $routeParams, $location) {
+            //values to add a new recipe
+        $scope.recipe = {};
+        $scope.recipe.ingredients = [];
+        $scope.recipe.steps = [];
+
+
+            dataService.getCategories(response =>{
+                $scope.categories = response.data;
+            });
+
+            $scope.redirectHome = () =>{
+                $location.url('/');
+            };
+            dataService.getFoodItems(response =>{
+                $scope.foodItems = response.data;
+            });
+
+        if($location.path() === '/edit/' + $routeParams.id) {
+            dataService.getRecipesByID($routeParams.id, response => {
+                $scope.recipe = response.data;
+            });
+        }
+            $scope.addIngredient = recipe => {
+                recipe.ingredients.push({
+                    "foodItem": "",
+                    "condition": "",
+                    "amount": ""
+                })
+            };
+
+            $scope.deleteIngredient = ($index) =>{
+                $scope.recipe.ingredients.splice($index, 1)
+            };
+            $scope.addStep = recipe =>{
+                recipe.steps.push({"description": ""})
+            };
+             $scope.deleteStep = ($index) =>{
+                $scope.recipe.steps.splice($index, 1)
+             };
+
+            $scope.saveRecipe = recipe =>{
+                console.log($location.url());
+                console.log(`/edit/${recipe._id}`);
+                if (`/edit/${recipe._id}` === $location.url()) {
+                    dataService.updateRecipe(recipe, (response) =>{
+                        console.log('edit');
+                        $scope.recipe = response.data;
+                        $location.url('/');
+                    });
+                }else{
+                    dataService.addRecipe(recipe, (response) =>{
+                        $scope.recipe = response.data;
+                        console.log('add');
+                        $location.url('/');
+                });
+
+                }
+
+
+        }
+        }
+})();
--- public/scripts/controllers/recipe.controller.js
+++ public/scripts/controllers/recipe.controller.js
@@ -7,35 +7,37 @@ angular
     function RecipesController($scope, dataService,$routeParams, $location) {
 
     dataService.getRecipes(function(response){
-        console.log(response.data);
         $scope.recipes = response.data;
     });
 
     dataService.getCategories(function(response){
-        console.log(response.data);
         $scope.categories = response.data;
     });
 
     $scope.getRecipesByCategory = function(category){
         if(category === "All Categories"){
+            console.log('all')
             dataService.getRecipes(function(response){
                 $scope.recipes = response.data
             });
         }else{
             dataService.getRecipesByCategory(category, function(response){
+                console.log(response.data)
                 $scope.recipes = response.data;
             })
         }
     };
 
-    $scope.addRecipe(function(){
-        $location.path('/add');
-    });
+    $scope.addRecipe = function(){
+        $location.url('/add');
+    };
 
-    $scope.deleteRecipe(function(recipe_id, $index){
-        dataService.deleteRecipe(recipe_id);
-        $scope.recipes.splice($index, 1);
-    });
+    $scope.deleteRecipe = function(id, $index){
+        dataService.deleteRecipeById(id, () =>{
+            $scope.recipes.splice($index, 1);
+        });
+
+    };
 
 }
 })();
